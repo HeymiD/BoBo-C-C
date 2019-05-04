6dd8a281bc712b59042a4b3b0b49a84c81d56f7b
HeymiD <35971205+HeymiD@users.noreply.github.com>
2019-05-03 21:36:14 -0400

    Update BoBo.py

--- master/BoBo.py
+++ master/BoBo.py
@@ -42,6 +42,6 @@ def initdb():
 
 
 if __name__ == '__main__':
-    #heroku_port = int(os.environ.get('PORT'))
-    app.run(host='0.0.0.0', port=8080)
+    heroku_port = int(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=heroku_part)
     #manager.run()
